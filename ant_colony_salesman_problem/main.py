0fff5f7e3d99f006d1d8e808570ec12e867a37b1
Santiago td <santiagotorreglosadiaz@gmail.com>
2023-04-26 23:57:46 -0500

    main.py ant colony problems

--- ant_colony_salesman_problem/main.py
+++ ant_colony_salesman_problem/main.py
@@ -6,8 +6,6 @@ import matplotlib.pyplot as plt
 import json
 from model import functions
 
-
-
 class AntColony(object):
 
     def __init__(self, distances, n_ants, n_best, n_iterations, decay, alpha=1, beta=1):
@@ -46,4 +44,45 @@ class AntColony(object):
             distance_logs.append(all_time_shortest_path[1])                      
         return all_time_shortest_path,distance_logs
 
-    
+    def spread_pheronome(self, all_paths, n_best, shortest_path):
+        sorted_paths = sorted(all_paths, key=lambda x: x[1])
+        for path, dist in sorted_paths[:n_best]:
+            for move in path:
+                self.pheromone[move] += 1.0 / self.distances[move]
+
+    def gen_path_dist(self, path):
+        total_dist = 0
+        for ele in path:
+            total_dist += self.distances[ele]
+        return total_dist
+
+    def gen_all_paths(self):
+        all_paths = []
+        for i in range(self.n_ants):
+            path = self.gen_path(0)
+            all_paths.append((path, self.gen_path_dist(path)))
+        return all_paths
+
+    def gen_path(self, start):
+        path = []
+        visited = set()
+        visited.add(start)
+        prev = start
+        for i in range(len(self.distances) - 1):
+            move = self.pick_move(self.pheromone[prev], self.distances[prev], visited)
+            path.append((prev, move))
+            prev = move
+            visited.add(move)
+        path.append((prev, start)) # going back to where we started    
+        return path
+
+    def pick_move(self, pheromone, dist, visited):
+        pheromone = np.copy(pheromone)
+        pheromone[list(visited)] = 0
+
+        row = (pheromone ** self.alpha) * (( 1.0 / dist) ** self.beta)
+
+        norm_row = row / row.sum()
+        move = np_choice(self.all_inds, 1, p=norm_row)[0]
+        return move
+
